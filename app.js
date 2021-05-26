c604e2a2c49f25c9308c5de04c1f06561f1f4b21
harsh-vish14 <harshkv9322@gmail.com>
2021-05-26 14:31:20 +0530

    connections to database done

--- app.js
+++ app.js
@@ -1,17 +1,60 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
+const shortid = require("shortid");
 const cors = require("cors");
+const validUrl = require("valid-url");
+const mongoose = require("mongoose");
+const app = express();
 const PORT = process.env.PORT || 8000;
+
+mongoose.connect(
+  `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@${process.env.CLUSTER}.ozhsq.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+const urlSchema = {
+  _id: String,
+  url: String,
+};
+
+const urlmongo = mongoose.model("short-urls", urlSchema);
 
 app.post("/register-url", (req, res) => {
+  // const id = shortid.generate();
   const { url } = req.body;
-  if (!url) {
+  if (!url || !validUrl.isUri(url)) {
     res.redirect("/notFound");
   }
+
+  urlmongo.findOne({ url }, (err, data) => {
+    if (data) {
+      res.status(200).json({
+        url: `http://localhost:8000/${data._id}`,
+      });
+    } else {
+      const id = shortid.generate();
+      if (id) {
+        urlmongo.insertMany({
+          _id: id,
+          url: url,
+        });
+      }
+      console.log(id);
+      res.status(200).json({
+        message: `http://localhost:8000/${id}`,
+      });
+    }
+  });
+});
+
+app.get("/:id", (req, res) => {
+  urlmongo.findById(req.params.id, (err, data) => {
+    res.redirect(data.url);
+  });
 });
 
 app.get("*", (req, res) => {
